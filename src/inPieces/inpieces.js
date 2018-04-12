5d33de9fc99b50d66cd5793c2ea59bc2f449db97
daialihexiaoniu <397698632@qq.com>
2018-04-12 11:17:10 +0800

    change inpieces

--- src/inPieces/inpieces.js
+++ src/inPieces/inpieces.js
@@ -2,30 +2,18 @@ import $ from "jquery";
 
 $(() => {
     const arr = [
-        'start-wrap', 
+        'pieces-wrap-0', 
         'pieces-wrap-1',
         'pieces-wrap-2',
         'pieces-wrap-3',
-        'pieces-wrap-4'
+        'pieces-wrap-4',
+        'pieces-wrap-5',
+        'pieces-wrap-6',
     ];
 
-    $('.J-button').on('click', () => {
-        $('.J-wrap').removeClass('start-wrap').addClass('pieces-wrap-1');
-    })
-
-    $('.J-button-1').on('click', () => {
-        $('.J-wrap').removeClass('pieces-wrap-1').addClass('pieces-wrap-2');
-    })
-
-    $('.J-button-2').on('click', () => {
-        $('.J-wrap').removeClass('pieces-wrap-2').addClass('pieces-wrap-3');
-    })
-
-    $('.J-button-3').on('click', () => {
-        $('.J-wrap').removeClass('pieces-wrap-3').addClass('pieces-wrap-4');
-    })
-
-    $('.J-button-4').on('click', () => {
-        $('.J-wrap').removeClass('pieces-wrap-4').addClass('pieces-wrap-5');
-    })
+    for(let i = 0, len = arr.length ; i < len; i++) {
+        $(`.J-button-${i - 1}`).on('click', () => {
+            $('.J-wrap').removeClass(arr[i - 1]).addClass(arr[i]);
+        })
+    }
 })
